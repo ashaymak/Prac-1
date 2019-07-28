afccf5778e3bb9486c9f58797f530aea1232f9f3
Ashay Makanjee <ashay.makanjee@gmail.com>
2019-07-28 23:02:25 +0200

    Neat code, lacking wrap for counter

--- Prac 1.py
+++ Prac 1.py
@@ -12,7 +12,6 @@ Date: 22/07/2019
 # import Relevant Librares
 import RPi.GPIO as GPIO
 import time
-status=1
 import itertools
 counter=0
 chan_list=(26,6,5)
@@ -22,39 +21,27 @@ GPIO.setup(5,GPIO.OUT)
 GPIO.setup(26,GPIO.OUT) 
 GPIO.setup(23,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(24,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-B=list(itertools.product([0,1], repeat=3))
+B=list(itertools.product([0,1], repeat=3)) 
 
 def main(ev=None):
     GPIO.add_event_detect(24, GPIO.FALLING, callback=button_up, bouncetime=200)
     GPIO.add_event_detect(23, GPIO.FALLING, callback=button_down, bouncetime=200)
     while True:
         time.sleep(1)
-   #for i in itertools.product([1,0], repeat=3):
-   #    GPIO.output(chan_list, i)
-    #   time.sleep(1)
-               
-#def swLed(ev=None):
-    
-    #global status
-    #status = not status
-    #GPIO.output(chan_list, status)  # switch led status(on-->off; off-->on)
-
+        
 def button_up(ev=None):
     global counter
-    
     counter=counter+1
     GPIO.output(chan_list, B[counter])
     
     
 def button_down(ev=None):
     global counter
-     # wait for falling and set bouncetime to prevent the callback function from being called multiple times when the button is pressed
     counter=counter-1
     GPIO.output(chan_list, B[counter])
-       # Don't do anything
+       
 
 if __name__ == "__main__":
-    # Make sure the GPIO is stopped correctly
     
     try:
         while True:
