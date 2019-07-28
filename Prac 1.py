d141c1212631facbd774f8ed13c21e195d5575fa
Ashay Makanjee <ashay.makanjee@gmail.com>
2019-07-28 22:36:06 +0200

    Full program without wraparound with counter

--- Prac 1.py
+++ Prac 1.py
@@ -17,45 +17,48 @@ import itertools
 counter=0
 chan_list=(26,6,5)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(6,GPIO.OUT,initial=GPIO.LOW)
-GPIO.setup(5,GPIO.OUT,initial=GPIO.LOW)
-GPIO.setup(26,GPIO.OUT,initial=GPIO.LOW) 
+GPIO.setup(6,GPIO.OUT)
+GPIO.setup(5,GPIO.OUT)
+GPIO.setup(26,GPIO.OUT) 
 GPIO.setup(23,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(24,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-B=itertools.product([0,1], repeat=3)
-def main():
-   for i in itertools.product([1,0], repeat=3):
-       GPIO.output(chan_list, i)
-       time.sleep(1)
-               
-def swLed(ev=None):
-    
-    global status
-    status = not status
-    GPIO.output(chan_list, status)  # switch led status(on-->off; off-->on)
+B=list(itertools.product([0,1], repeat=3))
 
-def button_up():
-    global counter
-    GPIO.add_event_detect(24, GPIO.FALLING, callback=swLed, bouncetime=200)
-    counter=counter+1
-    GPIO.output(chan_list, *B[counter])
+def main(ev=None):
+    GPIO.add_event_detect(24, GPIO.FALLING, callback=button_up, bouncetime=200)
+    GPIO.add_event_detect(23, GPIO.FALLING, callback=button_down, bouncetime=200)
     while True:
         time.sleep(1)
+   #for i in itertools.product([1,0], repeat=3):
+   #    GPIO.output(chan_list, i)
+    #   time.sleep(1)
+               
+#def swLed(ev=None):
     
-def button_down():
+    #global status
+    #status = not status
+    #GPIO.output(chan_list, status)  # switch led status(on-->off; off-->on)
+
+def button_up(ev=None):
     global counter
-    GPIO.add_event_detect(23, GPIO.FALLING, callback=swLed, bouncetime=200) # wait for falling and set bouncetime to prevent the callback function from being called multiple times when the button is pressed
+    
+    counter=counter+1
+    GPIO.output(chan_list, B[counter])
+    
+    
+def button_down(ev=None):
+    global counter
+     # wait for falling and set bouncetime to prevent the callback function from being called multiple times when the button is pressed
     counter=counter-1
     GPIO.output(chan_list, B[counter])
-    while True:
-        time.sleep(1)   # Don't do anything
+       # Don't do anything
 
 if __name__ == "__main__":
     # Make sure the GPIO is stopped correctly
-    #main()
+    
     try:
         while True:
-            button_up()
+         main()   
     except KeyboardInterrupt:
         print("Exiting gracefully")
         # Turn off your GPIOs here
